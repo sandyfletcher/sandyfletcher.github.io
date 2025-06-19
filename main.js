7d822f02b2a815afa43fbdbeba724b5258dddde0
sandy <patrick.a.r.fletcher@gmail.com>
2025-06-19 14:37:34 -0400

    simplify sidebar fetch logic, standardize link formatting, fixing mobile dch/svh/vh/%

--- main.js
+++ main.js
@@ -1,21 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebarPlaceholder = document.getElementById("sidebar-placeholder");
-    if (sidebarPlaceholder) { // calculate relative path to sidebar.html from current page
-        const pagePath = window.location.pathname;
-        let relativePathToRoot = ''; // count directory levels: /folder1/folder2/page.html -> ../../
-        const pathSegments = pagePath.substring(1).split('/');
-        if (pathSegments[0] !== "") { // not root directory
-            let levels = pathSegments.length -1; // is a dir if pathSegments has items and last is not empty / no dot
-            if (pagePath.endsWith('/') && pathSegments[pathSegments.length-1] !== "") {
-                levels = pathSegments.length;
-            } else if (pathSegments.length === 1 && pathSegments[0] === "") { // root path
-                levels = 0;
-            }
-            for (let i = 0; i < levels; i++) {
-                relativePathToRoot += '../';
-            }
-        }
-        const pathToSidebarHtmlFile = relativePathToRoot + 'sidebar.html';
+    if (sidebarPlaceholder) {
+        const pathToSidebarHtmlFile = '/sidebar.html'; // fetch sidebar.html from root folder
         fetch(pathToSidebarHtmlFile)
             .then(response => {
                 if (!response.ok) {
